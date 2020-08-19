4610d43dd3c0696aba6f09ac20c489abc95eaa53
unknown <18ucs081@lnmiit.ac.in>
2020-08-19 16:43:10 +0530

    Git commands

---
+++ index.js
@@ -0,0 +1,9 @@
+// git add -A
+// git commit -m ""
+// git push origin master
+// git pull origin master
+
+// git add remote
+
+// git config --global user.email "you@example.com"
+// git config --global user.name "Your Name"
