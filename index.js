83e8cbe97d2994e28b375366012cef6a1eb1066d
Dev Baweja <18ucs081@lnmiit.ac.in>
2021-01-23 23:41:20 +0530

    Configuring for npm

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-// git add -A
-// git commit -m ""
-// git push origin master
-// git pull origin master
+const additionalCipher = require('./src/additional-cipher');
+const multiplicationCipher = require('./src/multiplication-cipher');
+const affineCipher = require('./src/affine-cipher');
+const monoalphabeticCipher = require('./src/monoalphabetic-cipher');
+const autokeyCipher = require('./src/autokey-cipher');
+const playfairCipher = require('./src/playfair-cipher');
 
-// git add remote
-
-// git config --global user.email "you@example.com"
-// git config --global user.name "Your Name"
+module.exports = {
+    additionalCipher,
+    multiplicationCipher,
+    affineCipher,
+    monoalphabeticCipher,
+    autokeyCipher,
+    playfairCipher,
+};
